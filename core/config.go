e6fe45a3e070bec1034fe0a77293c6685ac84af5
Ronnie Smith <halfpint1170@gmail.com>
2021-06-27 23:41:55 -0700

    split config with custom

--- core/config.go
+++ core/config.go
@@ -2,108 +2,23 @@ package core
 
 import (
 	"bytes"
-	"errors"
-	"fmt"
-	"io"
 	"os"
 	"regexp"
-	"strconv"
 	"text/template"
 
 	"github.com/Masterminds/semver/v3"
 	"github.com/icholy/replace"
-	"github.com/manifoldco/promptui"
 	"github.com/miniscruff/changie/shared"
 	"golang.org/x/text/transform"
 	"gopkg.in/yaml.v2"
 )
 
 const (
-	ConfigPath   string     = ".changie.yaml"
-	CustomString CustomType = "string"
-	CustomInt    CustomType = "int"
-	CustomEnum   CustomType = "enum"
-)
-
-var (
-	ErrInvalidPromptType = errors.New("invalid prompt type")
-	errInvalidIntInput   = errors.New("invalid number")
-	errIntTooLow         = errors.New("input below minimum")
-	errIntTooHigh        = errors.New("input above maximum")
+	ConfigPath string = ".changie.yaml"
 )
 
 // GetVersions will return, in semver sorted order, all released versions
 type GetVersions func(shared.ReadDirer, Config) ([]*semver.Version, error)
-
-type enumWrapper struct {
-	*promptui.Select
-}
-
-func (e *enumWrapper) Run() (string, error) {
-	_, value, err := e.Select.Run()
-	return value, err
-}
-
-// Prompt is a small wrapper around the promptui Run method
-type Prompt interface {
-	Run() (string, error)
-}
-
-// CustomType determines the possible custom choice types
-type CustomType string
-
-// Custom contains the options for a custom choice for new changes
-type Custom struct {
-	Key         string
-	Type        CustomType
-	Label       string   `yaml:",omitempty"`
-	MinInt      *int64   `yaml:"minInt,omitempty"`
-	MaxInt      *int64   `yaml:"maxInt,omitempty"`
-	EnumOptions []string `yaml:"enumOptions,omitempty"`
-}
-
-// CreatePrompt will create a promptui select or prompt from a custom choice
-func (c Custom) CreatePrompt(stdinReader io.ReadCloser) (Prompt, error) {
-	label := c.Key
-	if c.Label != "" {
-		label = c.Label
-	}
-
-	switch c.Type {
-	case CustomString:
-		return &promptui.Prompt{
-			Label: label,
-			Stdin: stdinReader,
-		}, nil
-	case CustomInt:
-		return &promptui.Prompt{
-			Label: label,
-			Stdin: stdinReader,
-			Validate: func(input string) error {
-				value, err := strconv.ParseInt(input, 10, 64)
-				if err != nil {
-					return errInvalidIntInput
-				}
-				if c.MinInt != nil && value < *c.MinInt {
-					return fmt.Errorf("%w: %v < %v", errIntTooLow, value, c.MinInt)
-				}
-				if c.MaxInt != nil && value > *c.MaxInt {
-					return fmt.Errorf("%w: %v > %v", errIntTooHigh, value, c.MinInt)
-				}
-				return nil
-			},
-		}, nil
-	case CustomEnum:
-		return &enumWrapper{
-			Select: &promptui.Select{
-				Label: label,
-				Stdin: stdinReader,
-				Items: c.EnumOptions,
-			}}, nil
-	}
-
-	return nil, ErrInvalidPromptType
-}
 
 type ReplaceData struct {
 	Version         string
--- core/config_test.go
+++ core/config_test.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"os"
 
-	"github.com/manifoldco/promptui"
 	. "github.com/miniscruff/changie/test_utils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -136,92 +135,6 @@ custom:
 
 		_, err := LoadConfig(mockRf)
 		Expect(err).NotTo(BeNil())
-	})
-})
-
-var _ = Describe("Custom", func() {
-	It("returns error on invalid prompt type", func() {
-		_, err := Custom{
-			Type: "invalid type",
-		}.CreatePrompt(os.Stdin)
-		Expect(errors.Is(err, ErrInvalidPromptType)).To(BeTrue())
-	})
-
-	It("can create custom string prompt", func() {
-		prompt, err := Custom{
-			Type:  CustomString,
-			Label: "a label",
-		}.CreatePrompt(os.Stdin)
-		Expect(err).To(BeNil())
-
-		underPrompt, ok := prompt.(*promptui.Prompt)
-		Expect(ok).To(BeTrue())
-		Expect(underPrompt.Label).To(Equal("a label"))
-	})
-
-	It("can create custom int prompt", func() {
-		prompt, err := Custom{
-			Key:  "name",
-			Type: CustomInt,
-		}.CreatePrompt(os.Stdin)
-		Expect(err).To(BeNil())
-
-		underPrompt, ok := prompt.(*promptui.Prompt)
-		Expect(ok).To(BeTrue())
-		Expect(underPrompt.Validate("12")).To(BeNil())
-		Expect(underPrompt.Validate("not an int")).NotTo(BeNil())
-		Expect(underPrompt.Validate("12.5")).NotTo(BeNil())
-		Expect(underPrompt.Label).To(Equal("name"))
-	})
-
-	It("can create custom int prompt with a min and max", func() {
-		var min int64 = 5
-		var max int64 = 10
-		prompt, err := Custom{
-			Type:   CustomInt,
-			MinInt: &min,
-			MaxInt: &max,
-		}.CreatePrompt(os.Stdin)
-		Expect(err).To(BeNil())
-
-		underPrompt, ok := prompt.(*promptui.Prompt)
-		Expect(ok).To(BeTrue())
-		Expect(underPrompt.Validate("7")).To(BeNil())
-		Expect(errors.Is(underPrompt.Validate("3"), errIntTooLow)).To(BeTrue())
-		Expect(errors.Is(underPrompt.Validate("25"), errIntTooHigh)).To(BeTrue())
-	})
-
-	It("can create custom enum prompt", func() {
-		prompt, err := Custom{
-			Type:        CustomEnum,
-			EnumOptions: []string{"a", "b", "c"},
-		}.CreatePrompt(os.Stdin)
-		Expect(err).To(BeNil())
-
-		underPrompt, ok := prompt.(*enumWrapper)
-		Expect(ok).To(BeTrue())
-		Expect(underPrompt.Select.Items).To(Equal([]string{"a", "b", "c"}))
-	})
-
-	It("can run enum prompt", func() {
-		stdinReader, stdinWriter, err := os.Pipe()
-		Expect(err).To(BeNil())
-
-		defer stdinReader.Close()
-		defer stdinWriter.Close()
-
-		prompt, err := Custom{
-			Type:        CustomEnum,
-			EnumOptions: []string{"a", "b", "c"},
-		}.CreatePrompt(stdinReader)
-
-		go func() {
-			DelayWrite(stdinWriter, []byte{106, 13})
-		}()
-
-		out, err := prompt.Run()
-		Expect(err).To(BeNil())
-		Expect(out).To(Equal("b"))
 	})
 })
 
